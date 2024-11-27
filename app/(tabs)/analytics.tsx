3054f9bc31d57bf296ef0345b3fb57d76d44fee2
AGLOP-1354 <jake@jober.io>
2024-11-28 01:09:52 +0900

    [지표] 지표 페이지 blank page로 인한 앱 심사 거절 이슈 수정

--- app/(tabs)/analytics.tsx
+++ app/(tabs)/analytics.tsx
@@ -3,7 +3,8 @@ import { View, Text } from 'react-native';
 const Analytics = () => {
   return (
     <View>
-    <Text>Analytics</Text>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', color: 'white' }}>This is analytics Page</Text>
+      <Text style={{ fontSize: 16, color: 'white' }}>Prepare for your future</Text>
     </View>
   );
 };
